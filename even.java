55a87b63a8bdb9d7cb908768c6096354e77f6b8f
Varunann2 <varun090259@gmail.com>
2023-08-29 15:10:20 +0530

    this is my first file

---
+++ even.java
@@ -0,0 +1,16 @@
+class even
+{
+  public static void main(String args[])
+{
+char temp='a';
+int temp1=(int)temp;
+{
+if(temp1>=65)
+{
+   if(temp1<=90)
+     System.out.println("Capital letter");
+   else if(temp1<=122)
+    System.out.println("Small letter");
+   else
+   System.out.println("Not a alphabet");
+}
---
+++ lessgreat.java
@@ -0,0 +1,11 @@
+class lessgreat
+{
+   public static void main(String args[])
+{  
+     int a=222,b=200;
+     System.out.println(a>b);
+     System.out.println(a<b);
+     System.out.println(b=a);
+}
+}
+
