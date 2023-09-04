173b8a27ac99f17b15748ac7b2b8d291e0bd1b0b
Varunann2 <varun090259@gmail.com>
2023-09-04 16:01:44 +0530

    first

---
+++ area.java
@@ -0,0 +1,13 @@
+public class area
+{
+public static void main(String[]args)
+{
+int side=10;
+int length=20;
+int breadth=40;
+System.out.println("area of rectangle="+length*breadth);
+System.out.println("perimeter of rectangle="+2*length+breadth);
+System.out.println("area of square="+side*side);
+System.out.println("perimeter of square="+4*side);
+}
+}
