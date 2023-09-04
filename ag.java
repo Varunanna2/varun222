b47ce5a65c0c738cc3ee5d6c248b7b46312226fc
Varunann2 <varun090259@gmail.com>
2023-09-04 15:14:49 +0530

    second file

---
+++ ag.java
@@ -0,0 +1,19 @@
+class senior
+{
+  public static void main(String args[])
+{
+int age=9;
+if(age<18)
+{
+System.out.println("minor");
+}
+else if(age<50)
+{
+System.out.println("major");
+}
+else 
+{
+System.out.println("senior");
+}
+}
+}
---
+++ div.java
@@ -0,0 +1,10 @@
+class div
+{   
+   public static void main(String args[])
+ {
+   int a=676652,b=10;
+  System.out.println(a%b);
+  System.out.println(a/b);
+}
+}
+  
